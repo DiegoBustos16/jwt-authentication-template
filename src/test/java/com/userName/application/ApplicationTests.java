0c524f4a182b77f8a5efdd42fb98dfc4c880b0bf
DiegoBustos16 <diegombustos16@gmail.com>
2025-07-30 18:29:07 -0300

    Initial Commit

---
+++ src/test/java/com/userName/application/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userName.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
